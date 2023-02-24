c942de5550cca3925f53d1a3f5f96e6427e8967d
msclock <msclock@126.com>
2023-02-24 12:49:56 +0800

    style: change doxygen style from @ to \

--- src/app_api/src/api.hpp
+++ src/app_api/src/api.hpp
@@ -6,14 +6,14 @@
 namespace api {
 namespace info {
 /**
- * @brief get api version string
- * @note a string "dev" will be returned when no version configuration.
- * @return the distribution api version provided.
+ * \brief get api version string
+ * \note a string "dev" will be returned when no version configuration.
+ * \return the distribution api version provided.
  */
 std::string api_EXPORT version_api() noexcept;
 
 /**
- * @return a bool represents if debug when building.
+ * \return a bool represents if debug when building.
  */
 bool api_EXPORT is_debug() noexcept;
 }; // namespace info
