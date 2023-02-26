6f7799f0565236f408fcb91e53dbd7f5c4b5c2a6
msclock <msclock@126.com>
2023-02-27 02:35:42 +0800

    style: confront to precommit

--- CtestConfig.cmake
+++ CtestConfig.cmake
@@ -1,10 +1,9 @@
-## This file should be placed in the root directory of your project.
-## Then modify the CMakeLists.txt file in the root directory of your
-## project to incorporate the testing dashboard.
-##
-## # The following are required to submit to the CDash dashboard:
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
+# This file should be placed in the root directory of your project. Then modify
+# the CMakeLists.txt file in the root directory of your project to incorporate
+# the testing dashboard.
+#
+# # The following are required to submit to the CDash dashboard:
+# ENABLE_TESTING() INCLUDE(CTest)
 
 set(CTEST_PROJECT_NAME "cppblueprint")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
