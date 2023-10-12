56dba95b094a4cd0b39ae949816520917cab26b0
msclock <msclock@qq.com>
2023-10-12 18:21:44 +0800

    perf: use cmake-registry
    Close #4
    
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/ConfigureDocs.cmake
+++ cmake/ConfigureDocs.cmake
@@ -1,12 +1,12 @@
 # Build the target by issuing: cmake --build . --target
 # ${CMAKE_PROJECT_NAME}_docs
-list(APPEND CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake")
 
-include(ConfigDoxygen)
+include(cmake-modules/configure/ConfigDoxygen)
 configdoxygen()
 
 if(DOXYGEN_FOUND)
 
+  list(APPEND CMAKE_MODULE_PATH "${cmake-modules_MODULE_PATH}/configure")
   find_package(Sphinx REQUIRED breathe)
 
   set(DOXYGEN_GENERATE_HTML NO)
