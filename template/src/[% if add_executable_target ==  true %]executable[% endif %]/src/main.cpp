841be4dba9c3f0304dc15209921735508517ecd2
msclock <msclock@qq.com>
2023-10-30 17:10:21 +0800

    perf: organize executable example
    Add the testable ability
    
    Related #31
    
    Signed-off-by: msclock <msclock@qq.com>

--- template/src/[% if add_executable_target ==  true %]executable[% endif %]/src/main.cpp
+++ template/src/[% if add_executable_target ==  true %]executable[% endif %]/src/main.cpp
@@ -1,11 +1,12 @@
 ﻿#include "absl/log/globals.h"
 #include "absl/log/initialize.h"
+#include "absl/log/log.h"
 #include "git.h"
 #include "helpers.hpp"
 
 int main() {
     absl::InitializeLog();
     absl::SetStderrThreshold(absl::LogSeverity::kInfo);
-    LOG(INFO) << "Get a returned value: " << some_fun() << " ; Version: " << git::ProjectVersion();
+    LOG(INFO) << "Get a returned value: " << executable::helpers::some_fun() << " ; Version: " << git::ProjectVersion();
     return 0;
 }
