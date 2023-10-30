461bc21811368e850963b158182b134f4db63386
msclock <msclock@qq.com>
2023-10-30 17:10:21 +0800

    perf: organize executable example
    Add the testable ability
    
    Related #31
    
    Signed-off-by: msclock <msclock@qq.com>

--- src/executable/include/helpers.hpp
+++ src/executable/include/helpers.hpp
@@ -2,8 +2,12 @@
 
 #include "absl/log/log.h"
 
+namespace executable {
+namespace helpers {
 inline int some_fun() {
     LOG(INFO) << "Hello "
               << "helpers!";
     return 0;
 }
+} // namespace helpers
+} // namespace executable
