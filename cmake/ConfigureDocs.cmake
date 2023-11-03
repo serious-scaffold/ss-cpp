88027bf817a74503d158ef38d274911ae169fd45
msclock <msclock@qq.com>
2023-11-03 16:43:17 +0800

    fix: error on the lack of sphinx tools
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/ConfigureDocs.cmake
+++ cmake/ConfigureDocs.cmake
@@ -6,11 +6,10 @@ configdoxygen()
 
 if(DOXYGEN_FOUND)
 
-  list(APPEND CMAKE_MODULE_PATH "${cmake-modules_MODULE_PATH}/configure")
-  find_package(Sphinx REQUIRED breathe)
-
+  # Generate docs needs XML OUTPUT with sphinx
   set(DOXYGEN_GENERATE_HTML NO)
   set(DOXYGEN_GENERATE_XML YES)
+
   # cmake-format: off
   doxygen_add_docs(
     ${CMAKE_PROJECT_NAME}_doxygen
@@ -22,17 +21,23 @@ if(DOXYGEN_FOUND)
     COMMENT "gen doxygen style docs automatically")
   # cmake-format: on
 
-  sphinx_add_docs(
-    ${CMAKE_PROJECT_NAME}_docs
-    BREATHE_PROJECTS
-    ${CMAKE_PROJECT_NAME}_doxygen
-    BUILDER
-    html
-    # BREATH_DEBUG True
-    CONF_FILE
-    ${PROJECT_SOURCE_DIR}/docs/conf.py
-    SOURCE_DIRECTORY
-    docs
-    OUTPUT_DIRECTORY
-    ${CMAKE_PROJECT_NAME}_docs)
+  if(TARGET ${CMAKE_PROJECT_NAME}_doxygen)
+    list(APPEND CMAKE_MODULE_PATH "${cmake-modules_MODULE_PATH}/configure")
+    find_package(Sphinx REQUIRED breathe)
+
+    sphinx_add_docs(
+      ${CMAKE_PROJECT_NAME}_docs
+      BREATHE_PROJECTS
+      ${CMAKE_PROJECT_NAME}_doxygen
+      BUILDER
+      html
+      BREATH_DEBUG
+      False
+      CONF_FILE
+      ${PROJECT_SOURCE_DIR}/docs/conf.py
+      SOURCE_DIRECTORY
+      docs
+      OUTPUT_DIRECTORY
+      ${CMAKE_PROJECT_NAME}_docs)
+  endif()
 endif()
