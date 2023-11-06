e1ae2d03357dc8acdae90be1c2d2477954769658
msclock <msclock@qq.com>
2023-11-06 18:52:41 +0800

    fix: jni target are always added
    Signed-off-by: msclock <msclock@qq.com>

--- src/jni/tests/java/MyTest.java
+++
@@ -1,8 +0,0 @@
-public class MyTest {
-     private static int magic_counter=777;
-
-     public static void mymain() {   // <=== We will call this
-         System.out.println("Hello, World in java from mymain");
-         System.out.println(magic_counter);
-     }
-}
