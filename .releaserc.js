0e784776b2bc328e42dd40133aaa984db33d5b3f
msclock <msclock@qq.com>
2024-03-19 10:47:50 +0800

    perf: remove changelog generation

--- .releaserc.js
+++ .releaserc.js
@@ -57,14 +57,6 @@ module.exports = {
         ].join(" && "),
       }
     ],
-    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-      },
-    ],
     "@semantic-release/github",
   ],
 };
