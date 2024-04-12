31bbb5a94692976b836605427b0a081cfd9d1420
msclock <msclock@qq.com>
2024-04-12 11:46:17 +0800

    perf: add automatic build type initialization (#201)
    Signed-off-by: msclock <msclock@qq.com>

---
+++ cmake/vcpkg/scripts/build_type.cmake
@@ -0,0 +1,28 @@
+#[[
+SPDX-License-Identifier: MIT
+SPDX-FileCopyrightText: Copyright 2024 msclock
+
+This module initializes the build type.
+]]
+
+if(NOT CMAKE_BUILD_TYPE)
+  message(
+    STATUS "Setting CMAKE_BUILD_TYPE to 'RelWithDebInfo' as none was specified."
+  )
+endif()
+set(CMAKE_BUILD_TYPE
+    RelWithDebInfo
+    CACHE STRING "Choose the type of build.")
+
+# Set the possible values of build type for cmake-gui, ccmake
+get_property(_builtin_build_types GLOBAL PROPERTY CMAKE_BUILD_TYPE)
+
+foreach(_type "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+  if(NOT _type IN_LIST _builtin_build_types)
+    list(APPEND _builtin_build_types ${_type})
+  endif()
+endforeach()
+
+set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS ${_builtin_build_types})
+
+unset(_builtin_build_types)
