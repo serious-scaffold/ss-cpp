0c60c5aa34e763e50d2938ed9ff66cb0e14bf5f5
msclock <msclock@qq.com>
2024-05-14 05:45:52 +0800

    fix: version should retrieve from tags output
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/ConfigureVersion.cmake
+++ cmake/ConfigureVersion.cmake
@@ -13,15 +13,22 @@ find_package(robotology-cmake-ycm REQUIRED)
 include(robotology-cmake-ycm/modules/GitInfo)
 include(robotology-cmake-ycm/modules/ExtractVersion)
 
+# Retrieve the git commit information to CMAKE_PROJECT* variables
 git_commit_info(SOURCE_DIR ${CMAKE_SOURCE_DIR} PREFIX CMAKE_PROJECT)
-if(NOT CMAKE_PROJECT_GIT_COMMIT_DESCRIBE)
+
+if(NOT CMAKE_PROJECT_GIT_COMMIT_TAG)
   set(CMAKE_PROJECT_VERSION "0.0.0.0")
 else()
-  string(REGEX MATCH [[v?(.*)$]] matches_out
-               "${CMAKE_PROJECT_GIT_COMMIT_DESCRIBE}")
-  set(CMAKE_PROJECT_VERSION ${CMAKE_MATCH_1})
-  string(REPLACE "-" "." CMAKE_PROJECT_VERSION ${CMAKE_PROJECT_VERSION})
+  set(CMAKE_PROJECT_VERSION "${CMAKE_PROJECT_GIT_COMMIT_TAG}")
+
+  if(CMAKE_PROJECT_GIT_COMMIT_TAG_REVISION)
+    set(CMAKE_PROJECT_VERSION
+        "${CMAKE_PROJECT_VERSION}.${CMAKE_PROJECT_GIT_COMMIT_TAG_REVISION}")
+  endif()
+
+  string(REGEX REPLACE "^v" "" CMAKE_PROJECT_VERSION "${CMAKE_PROJECT_VERSION}")
 endif()
 
+# Set major, minor, patch and tweak version numbers
 extract_version(CMAKE_PROJECT CMAKE_PROJECT REVERSE_NAME)
 generate_git_header()
