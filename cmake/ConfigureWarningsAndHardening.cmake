ab7e2329948c109d5e2fc45b1a46d0bf14a29c62
msclock <msclock@qq.com>
2024-05-22 17:41:07 +0800

    perf: optimize cmake modules default vars (#258)
    Move cmake default variables from cmakepreset json to cmake modules and
    simplify relevant ci jobs.
    
    ---------
    
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/ConfigureWarningsAndHardening.cmake
+++ cmake/ConfigureWarningsAndHardening.cmake
@@ -1,9 +1,57 @@
 #[[
-This file configures the compiler flags, hardening options, dynamic and static tools for the project.
+This file configures the following things:
+  - dynamic tools: sanitizers, valgrind.
+  - static tools: clang-tidy, cppcheck.
+  - compiler flags
+  - hardening options
 
 ]]
 
 include_guard(GLOBAL)
+
+# ##############################################################################
+# Sanitizer
+# ##############################################################################
+
+set(USE_SANITIZER
+    OFF
+    CACHE BOOL "Enable sanitizer")
+
+include(cmake-modules/build/Sanitizer)
+
+# ##############################################################################
+# Valgrind
+# ##############################################################################
+
+set(USE_VALGRIND
+    OFF
+    CACHE BOOL "Enable Valgrind")
+
+include(cmake-modules/test/Valgrind)
+
+# ##############################################################################
+# Clang-Tidy
+# ##############################################################################
+
+set(USE_CLANGTIDY
+    OFF
+    CACHE BOOL "Enable Clang-Tidy")
+
+include(cmake-modules/build/ClangTidy)
+
+# ##############################################################################
+# Cppcheck
+# ##############################################################################
+
+set(USE_CPPCHECK
+    OFF
+    CACHE BOOL "Enable Cppcheck")
+set(USE_CPPCHECK_SUPPRESSION_FILE
+    ${CMAKE_SOURCE_DIR}/.cppcheck-suppressions.txt
+    CACHE STRING
+          "Customize the path to the Cppcheck suppressions file of the project")
+
+include(cmake-modules/build/Cppcheck)
 
 # ##############################################################################
 # CompilerFlags
@@ -16,31 +64,3 @@ include(cmake-modules/build/CompilerFlags)
 # ##############################################################################
 
 include(cmake-modules/build/Hardening)
-
-# ##############################################################################
-# Sanitizer
-# ##############################################################################
-
-include(cmake-modules/build/Sanitizer)
-
-# ##############################################################################
-# Valgrind
-# ##############################################################################
-
-include(cmake-modules/test/Valgrind)
-
-# ##############################################################################
-# Clang-Tidy
-# ##############################################################################
-
-include(cmake-modules/build/ClangTidy)
-
-# ##############################################################################
-# Cppcheck
-# ##############################################################################
-
-# Customize the path to the Cppcheck suppressions file of the project.
-set(USE_CPPCHECK_SUPPRESSION_FILE
-    ${CMAKE_SOURCE_DIR}/.cppcheck-suppressions.txt)
-
-include(cmake-modules/build/Cppcheck)
