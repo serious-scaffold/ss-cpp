7e90eeb63131d09cd2a8da713aedcb1bb96be01f
msclock <msclock@qq.com>
2024-07-18 21:06:37 +0800

    perf: use native sphinx commands to generate docs (#317)
    Signed-off-by: msclock <msclock@qq.com>

--- cmake/ConfigureDocs.cmake
+++ cmake/ConfigureDocs.cmake
@@ -2,10 +2,6 @@
 This file creates targets to generate documentation:
 
    - Doxygen: ${CMAKE_PROJECT_NAME}-doxygen: generates doxygen output.
-   - Sphinx:
-       - ${CMAKE_PROJECT_NAME}-docs: generates HTML documentation.
-       - ${CMAKE_PROJECT_NAME}-docs-check: generates HTML documentation with warnings as errors.
-       - ${CMAKE_PROJECT_NAME}-docs-linkcheck: checks links in HTML documentation
 ]]
 
 include(cmake-modules/configure/ConfigDoxygen)
@@ -39,53 +35,4 @@ if(DOXYGEN_FOUND)
     "${PROJECT_SOURCE_DIR}/src/exe/include"
     "${PROJECT_SOURCE_DIR}/src/header/include")
   # cmake-format: on
-
-  if(TARGET ${doxygen_docs})
-    set(Sphinx_FIND_COMPONENTS breathe)
-    include(cmake-modules/configure/FindSphinx)
-    unset(Sphinx_FIND_COMPONENTS)
-
-    set(DOCS_CHECK_NITPICKY_MODE
-        OFF
-        CACHE BOOL "Nitpicky mode for Sphinx documentation")
-
-    set(_common_sphinx_args
-        CONF_FILE
-        ${PROJECT_SOURCE_DIR}/docs/conf.py
-        BREATHE_PROJECTS
-        ${doxygen_docs}
-        BREATH_DEBUG
-        False
-        SOURCE_DIRECTORY
-        docs)
-
-    sphinx_add_docs(
-      ${CMAKE_PROJECT_NAME}-docs
-      BUILDER
-      html
-      EXTRA_ARGS
-      -T
-      OUTPUT_DIRECTORY
-      ${CMAKE_SOURCE_DIR}/docs/_build/html
-      ${_common_sphinx_args})
-
-    sphinx_add_docs(
-      ${CMAKE_PROJECT_NAME}-docs-check
-      BUILDER
-      html
-      EXTRA_ARGS
-      $<$<BOOL:${DOCS_CHECK_NITPICKY_MODE}>:-n>
-      -T
-      --keep-going
-      -W
-      OUTPUT_DIRECTORY
-      ${CMAKE_SOURCE_DIR}/docs/_build/html
-      ${_common_sphinx_args})
-
-    sphinx_add_docs(
-      ${CMAKE_PROJECT_NAME}-docs-linkcheck BUILDER linkcheck OUTPUT_DIRECTORY
-      ${CMAKE_SOURCE_DIR}/docs/_build/linkcheck ${_common_sphinx_args})
-
-    unset(_common_sphinx_args)
-  endif()
 endif()
